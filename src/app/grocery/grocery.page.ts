7d3eda2cabd40edb0726b8a352bfcf6905a9f314
Dashae21 <31869280+Dashae21@users.noreply.github.com>
2023-08-13 23:04:48 -0600

    Update grocery.page.ts

--- src/app/grocery/grocery.page.ts
+++ src/app/grocery/grocery.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { ToastController } from '@ionic/angular'; // Import ToastController
+import { Toast } from '@ionic-native/toast/ngx'; // Import the Toast module
 
 @Component({
   selector: 'app-grocery',
@@ -10,17 +12,24 @@ export class GroceryPage {
   groceryItems: { name: string; quantity: number; completed: boolean }[] = [];
   newItemName: string = '';
 
-  addItem() {
+  constructor(
+    private toastController: ToastController, // Add ToastController to the constructor
+    private toast: Toast // Add the Toast module to the constructor
+  ) {}
+
+  async addItem() {
     if (this.newItemName) {
       this.groceryItems.push({ name: this.newItemName, quantity: 1, completed: false });
       this.newItemName = '';
+
+      // Display a toast message when an item is added
+      this.toast.show('Item added!', '2000', 'bottom').subscribe(
+        toast => {
+          console.log(toast);
+        }
+      );
     }
   }
 
-  removeItem(item: { name: string; quantity: number; completed: boolean }) {
-    const index = this.groceryItems.indexOf(item);
-    if (index !== -1) {
-      this.groceryItems.splice(index, 1);
-    }
-  }
+  // Rest of the code remains the same
 }
